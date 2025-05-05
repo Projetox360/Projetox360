b0aea086e1fd96a8ac18dbde6342591cddc930ec
Projetox360 <kay.souza.z07@gmail.com>
2025-05-04 22:12:26 -0300

    Update script.js
    Scripts ( 22:12)

--- script.js
+++ script.js
@@ -1,25 +1,3 @@
-const audio = document.getElementById('introAudio');
-const audioControl = document.getElementById('audio-control');
-
-let isMuted = false;
-
-function toggleAudio() {
-  isMuted = !isMuted;
-  audio.muted = isMuted;
-  audioControl.textContent = isMuted ? '🔇' : '🔊';
-}
-
-function startIntro() {
-  audio.volume = 1.0;
-  audio.play();
-
-  // Fade out do áudio e redirecionamento
-  const fadeOut = setInterval(() => {
-    if (audio.volume > 0.1) {
-      audio.volume -= 0.02;
-    } else {
-      clearInterval(fadeOut);
-      window.location.href = "login.html"; // Redireciona para a página de login
-    }
-  }, 800);
-}
+setTimeout(() => {
+  window.location.href = "login.html";
+}, 5000); // redireciona para login após 5 segundos da intro
