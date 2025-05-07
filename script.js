a163f1a1b3ca26fb063efb51fe0c10f5fb88b05c
Projetox360 <kay.souza.z07@gmail.com>
2025-05-06 21:46:19 -0300

    Update script.js
    Novo script

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
-setTimeout(() => {
-  window.location.href = "login.html";
-}, 5000); // redireciona para login após 5 segundos da intro
+function allowAccess() {
+  document.getElementById("age-verification").style.display = "none";
+}
+
+function denyAccess() {
+  alert("Você precisa ter mais de 18 anos para acessar.");
+  window.location.href = "https://google.com";
+}
+
+document.getElementById("filters").addEventListener("submit", function(e) {
+  e.preventDefault();
+  alert("Busca ativada! Em breve, resultados reais serão exibidos.");
+});
